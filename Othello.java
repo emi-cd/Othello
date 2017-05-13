5291bcd3c5ecce36e56eb1c54b7b9f464fa7d63b
Emi <emi02cd@gmail.com>
2017-05-13 15:20:28 +0900

    入力を受け付ける

--- Othello.java
+++ Othello.java
@@ -1,44 +1,103 @@
-
+import java.util.*;
 
 
 class Othello {
 	// ボード 0:空,1:user,2:cpu
-	String bord[][] = new String[8][9];
+	int bord[][];
+	private static final int WALL = -1, EMPTY =  0, ME = 1, CPU = 2;
 
 	// コンストラクタ
 	Othello(){
 		// 配列bordの初期化
-		for (int i=0; i<8; i++) {
-			for (int j=0; j<8; j++){
-				if ((i == 3 && j == 4) || (i == 4 && j == 3))
-					bord[i][j] = "○";
-				else if ((i == 3 && j == 3) || (i == 4 && j == 4))
-					bord[i][j] = "";
-				else
-					bord[i][j] = "*";
-			}
-			bord[i][8] = "\n";
-		}
+		this.bord = new int[10][10];
+		int bord[][] = {
+			{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+			{-1,0,0,0,0,0,0,0,0,-1},
+			{-1,0,0,0,0,0,0,0,0,-1},
+			{-1,0,0,0,0,0,0,0,0,-1},
+			{-1,0,0,0,1,2,0,0,0,-1},
+			{-1,0,0,0,2,1,0,0,0,-1},
+			{-1,0,0,0,0,0,0,0,0,-1},
+			{-1,0,0,0,0,0,0,0,0,-1},
+			{-1,0,0,0,0,0,0,0,0,-1},
+			{-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+		};
+		this.bord = bord;
 	}
 
 	public static void main(String[] args) {
 		Othello othello = new Othello();
+		int move[];
 
 		// while(1){
 			othello.write();
-			// move = userInput();
-			// turnStone(move);
-			// mpve = cpu();
+			move = othello.userInput();
+			//othello.turnStone();
+			//othello.write();
+			// move = cpu();
 			// turnStone(move);
 		// }
 	}
 
 	// Userの入力を受け取る
-	//public String userInput(){}
+	public int[] userInput() {
+		System.out.print("please input like 3a. >  ");
+		Scanner sc = new Scanner(System.in);
+		String move = sc.next();
+
+		int[] ret = {0,0};
+
+		char rowName[] = {'a','b','c','d','e','f','g','h'};
+		Map<Character,Integer> row_num = new HashMap<Character,Integer>();
+		int counter = 1;
+		for(char name :rowName){
+			row_num.put(name,counter);
+			counter++;
+		}
+		try{
+			ret[0] = Character.getNumericValue(move.charAt(0));
+			ret[1] = row_num.get(move.charAt(1));
+		}catch(NullPointerException e){	ret = userInput();	}
+		if((ret[0] < 1 || 8 < ret[0] ) || (ret[1] < 1 || 8 < ret[1] )) {
+			ret = userInput();
+		}
+		if(!putCheck(ret,ME)){
+			ret = userInput();
+		}
+
+		return ret;
+
+	}
 	// CPUの手を返す
 	//public String cpu(){}
-	// 石をひっくり返す
-	// public void turnStone(String move){}
+	// 石をひっくり返せるかcheck
+	public boolean putCheck(int move[],int player) {
+		int[][] dir = {{-1,-1},{0,-1},{1,-1},{1,0},{1,1},{0,1},{-1,1},{-1,0}};
+		int row = move[0];
+		int colum = move[1];
+		// 置き場所が空か
+		if (bord[row][colum] != EMPTY) {	return false;	}
+
+		// ひっくり返せるか
+		for(int i = 0; i < dir.length; i++) {
+			int X = dir[i][0];
+			int Y = dir[i][1];
+			int tmpRow = row;
+			int tmpColum = colum;
+			boolean flag = false;
+			while(bord[tmpRow][tmpColum] >= 0){
+				if (bord[tmpRow += X][tmpColum += Y] == 3 - player) {
+					flag = true;
+				}
+				else if (flag && (bord[tmpRow][tmpColum] == player)) {
+					return true;
+				}else
+					break;
+			}
+		}
+		return false;
+	}
+
 
 
 	// ボードを表示する
@@ -49,11 +108,17 @@ class Othello {
 			System.out.print(" " + c );
 		}
 
-		for (int i=0; i<8; i++) {
-			System.out.print((i+1) + " ");
-			for (int j=0; j<9; j++){
-				System.out.print(" " + bord[i][j]);
+		for (int i=1; i<9; i++) {
+			System.out.print(i + " ");
+			for (int j=1; j<9; j++){
+				if(bord[i][j] == EMPTY)
+					System.out.print(" *");
+				else if (bord[i][j] == ME) 
+					System.out.print(" ○");
+				else
+					System.out.print(" ×");
 			}
+			System.out.println("");
 		}
 
 
